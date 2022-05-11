58b58e8a70e81350d9a20a56a6abff67e963156f
Exelion <autodiagbin@gmail.com>
2022-05-11 10:05:14 +0200

    recuperation fichiers perdus

--- project.py
+++ project.py
@@ -1,24 +1,29 @@
-
 import os
+from  datetime import date
 import json
 import logging
 
-from pprint import pprint
 
 CUR_DIR = os.path.abspath(os.getcwd())
 DATA_DIR = os.path.join(CUR_DIR,"data")
 FOLDERS_DATABASE = os.path.join(DATA_DIR,"arborescence_dossier.json")
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 class Project:
-    def __init__(self,marque:str,modele:str,autodiag:str,specifite:list[str],options:list[str],path:str) -> None:
+    def __init__(self,marque:str,modele:str,autodiag:str,specifite:list[str],options:list[str],path:str,printcpt:str = "n/a",version:str = "n/a", type_objet:list[str]=[]) -> None:
         self.marque = marque
         self.modele = modele
         self.autodiag = autodiag
         self.specifite = specifite
         self.options = options
         self.path = path
+        self.printcpt = printcpt
+        self.creator = self.getUser()
+        self.date_created = date.today().strftime('%m/%d/%y')
+        self.version = version
+        self.type_objet = type_objet
+
 
     def __str__(self) -> str:
         return f"Dossier projet : {self.marque} {self.modele} - Autodiag : {self.autodiag} OPT : {self.specifite} avec options {self.options} - Destination : {self.path}"
@@ -31,11 +36,16 @@ class Project:
             with open(FOLDERS_DATABASE,"r") as fd:
                 data = json.load(fd)
                 for dir_root in data:
-                    path = os.path.join(CUR_DIR,dir_root)
-                    os.makedirs(path,exist_ok=True)
-                    for dirs in data[dir_root]:
-                        os.makedirs(os.path.join(path,dirs),exist_ok=True)
-                
+                    if self.path != "" or self.path != None:
+                        path = os.path.join(self.path,dir_root)
+                        os.makedirs(path,exist_ok=True)
+                        for dirs in data[dir_root]:
+                            os.makedirs(os.path.join(path,dirs),exist_ok=True)
+                    else:
+                        logging.warning("ERREUR 102 : Probléme de DIRECTORY. Absent ou inexistant")
+                with open(os.path.join(self.path,"nfo.json"),"w") as js:
+                    json.dump(fp=js,obj=self.__dict__,indent=4)
+        
         else:
             logging.warning("ERREUR 101 : Le fichier d'arborescence de fichier n'existe pas")
 
@@ -54,6 +64,14 @@ def get_specificite():
         with open(directory,"r",encoding='utf8') as db:
             f = db.read().splitlines()
             logging.info("COLLECT : Collecter les spécificité")
+            return f
+
+def get_options():
+    directory = os.path.join(DATA_DIR,"options.csv")
+    if os.path.isfile(directory):
+        with open(directory,"r",encoding='utf8') as db:
+            f = db.read().splitlines()
+            logging.info("COLLECT : Collecter les options")
             return f
 
 def get_autodiags():
@@ -76,5 +94,6 @@ def get_capteurs():
 
 
 if __name__ == "__main__":
-    p = Project("Samsung","Galaxy S21FE","Bouygues",["5G","eSim"],["Empreinte Façade"],"")
+    pass
+    
 
